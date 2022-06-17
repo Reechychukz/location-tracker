aad9d9327c8d6e557a2ecd8239929723d5d15996
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-06-18 00:05:35 +0100

    Update rapidAPI.js

--- utilities/rapidAPI.js
+++ utilities/rapidAPI.js
@@ -5,7 +5,7 @@ const options = {
     url: 'https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation',
     params: {apikey: '873dbe322aea47f89dcf729dcc8f60e8'},
     headers: {
-      'X-RapidAPI-Key': '46600eab7emsh27985f7eeb5e93cp16a87cjsn03bb3c5645bb',
+      'X-RapidAPI-Key': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
       'X-RapidAPI-Host': 'find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com'
     }
 };
